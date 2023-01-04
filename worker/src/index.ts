9a32716eb7d3ad64391ee7ae90ec81c9a9c4b5e7
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2023-01-03 23:20:48 -0500

    Update index.ts

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -9,19 +9,19 @@
  */
 
 export interface Env {
-	GIF_BUCKET: R2Bucket;
-}
+  GIF_BUCKET: R2Bucket;
+};
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		const index = Math.floor(Math.random() * 1000);
-		const object = await env.GIF_BUCKET.get(`${index}.gif`);
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const index = Math.floor(Math.random() * 1000);
+    const object = await env.GIF_BUCKET.get(`${index}.gif`);
 
-		if (!object) return new Response('not found', { status: 404, headers: { 'content-type': 'text/html; charset=UTF-8' } });
+    if (!object) return new Response('not found', { status: 404, headers: { 'content-type': 'text/html; charset=UTF-8' } });
 		const data = await object.arrayBuffer();
 
 		return new Response(data, {
