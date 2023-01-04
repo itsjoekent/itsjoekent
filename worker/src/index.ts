9228a3b61eda652f839e81d9ef927814e61ea2cf
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2023-01-03 23:21:53 -0500

    Update index.ts

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -13,15 +13,15 @@ export interface Env {
 };
 
 export default {
-  async fetch(
-    request: Request,
-    env: Env,
-    ctx: ExecutionContext
-  ): Promise<Response> {
-    const index = Math.floor(Math.random() * 1000);
-    const object = await env.GIF_BUCKET.get(`${index}.gif`);
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<Response> {
+		const index = Math.floor(Math.random() * 1000);
+		const object = await env.GIF_BUCKET.get(`${index}.gif`);
 
-    if (!object) return new Response('not found', { status: 404, headers: { 'content-type': 'text/html; charset=UTF-8' } });
+		if (!object) return new Response('not found', { status: 404, headers: { 'content-type': 'text/html; charset=UTF-8' } });
 		const data = await object.arrayBuffer();
 
 		return new Response(data, {
