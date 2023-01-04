e26cc9f3b310448f4f5e91131ea31af909c630ba
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2023-01-03 23:23:29 -0500

    Update index.ts

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -13,22 +13,22 @@ export interface Env {
 };
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		const index = Math.floor(Math.random() * 1000);
-		const object = await env.GIF_BUCKET.get(`${index}.gif`);
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const index = Math.floor(Math.random() * 1000);
+    const object = await env.GIF_BUCKET.get(`${index}.gif`);
 
-		if (!object) return new Response('not found', { status: 404, headers: { 'content-type': 'text/html; charset=UTF-8' } });
-		const data = await object.arrayBuffer();
+    if (!object) return new Response('not found', { status: 404, headers: { 'content-type': 'text/html; charset=UTF-8' } });
+    const data = await object.arrayBuffer();
 
-		return new Response(data, {
-			headers: {
-				'cache-control': 'max-age=0, no-cache, no-store, must-revalidate',
-				'content-type': 'image/gif',
-			},
-		});
-	},
+    return new Response(data, {
+      headers: {
+        'cache-control': 'max-age=0, no-cache, no-store, must-revalidate',
+        'content-type': 'image/gif',
+      },
+    });
+  },
 };
